580dc5c555e0c9a6fd0bb62c31c78bf891356df2
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-30 00:46:03 +0900

    [20.4.9] usePreloader() hook 작성 & 사용

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,23 +1,19 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { Preloader } from "../lib/PreloadContext"
+import { usePreloader } from "../lib/PreloadContext"
 import { getUser } from "../modules/users"
 import User from "../components/User"
 
 const UserContainer = ({ id }) => {
   const user = useSelector(state => state.users.user)
   const dispatch = useDispatch()
-
+  usePreloader(() => dispatch(getUser(id))) // 서버 사이드 렌더링을 할 때 API 호출하기
   useEffect(() => {
     if (user?.id === parseInt(id, 10)) return // 사용자가 존재하고, id가 일치한다면 요청하지 않음
     dispatch(getUser(id))
   }, [dispatch, id, user])
 
-  if (!user) {
-    // 컨테이너 유효성 검사 후 return null을 해야 하는 경우에
-    // null 대신 Preloader 반환
-    return <Preloader resolve={() => dispatch(getUser(id))} />
-  }
+  if (!user) return null
   return <User user={user} />
 }
 
